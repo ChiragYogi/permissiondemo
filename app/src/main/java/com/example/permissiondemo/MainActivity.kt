b02368253f69de57b87db8b693aca0f3b489f427
ChiragYogi <chirag433@gmail.com>
2023-03-05 16:13:01 +0530

    image picker code added

--- app/src/main/java/com/example/permissiondemo/MainActivity.kt
+++ app/src/main/java/com/example/permissiondemo/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.example.permissiondemo
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
+import com.example.permissiondemo.databinding.ActivityMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var latestTmpUri: Uri? = null
+
+    val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { isSucess ->
+            if (isSucess) {
+                latestTmpUri?.let {
+                    Glide.with(this).load(it).into(binding.imageView)
+
+                }
+            }
+
+        }
+
+    val galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { data ->
+
+            data?.let {
+                latestTmpUri = it
+                Glide.with(this).load(latestTmpUri).into(binding.imageView)
+            }
+        }
+
+
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonClick.setOnClickListener {
+            getTmpFileUri().let { uri ->
+                latestTmpUri = uri
+                cameraLauncher.launch(latestTmpUri)
+            }
+        }
+
+        binding.buttonClickGallery.setOnClickListener {
+            galleryLauncher.launch("image/*")
+        }
+    }
+
+
+
+    private fun getTmpFileUri(): Uri {
+        val tmpFile =
+            File.createTempFile("tmp_image_file", ".png", this.cacheDir).apply {
+                createNewFile()
+                deleteOnExit()
+            }
+
+        return FileProvider.getUriForFile(
+            this.applicationContext,
+            "com.example.permissiondemo.provider",
+            tmpFile
+        )
     }
 }
